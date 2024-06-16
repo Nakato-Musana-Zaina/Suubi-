150363b2c48e8c2c7b75e263b68d5b2b04ea83dc
kimura-khamali <kimurakhamali@gmail.com>
2024-06-16 11:05:27 +0300

    working on product

--- src/App.js
+++ src/App.js
@@ -5,13 +5,10 @@ import Product from './product/product';
 function App() {
   return (
     <div className="App">
-      <h1>Live and learn</h1>
+      
       <Product/>
     </div>
-    //  <div className = "container1">
-    //  <div className ="left-row">
-    //    <MovieRatingList />
-    //  </div>
+  
   );
 }
 
