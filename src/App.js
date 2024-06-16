f2459e27d36f1fed34ac7201d61ba0a981ca1d11
Lavender <anyangolavenderr@gmail.com>
2024-06-16 22:50:09 +0300

    landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import Navbar from './Landing/landing';
 
-function About() {
+function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      {/* Other components go here */}
     </div>
   );
 }
 
-export default About;
+export default App;
